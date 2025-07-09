f88ad052acae2424704b8c86d8e0aea41b56abb2
Yash Patel <yppatel21306@gmail.com>
2025-07-09 15:43:21 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+// alert(3+3)   // we are using nodejs, not browser
+
+let name = "YASH"
+let age = 19
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+// object
+
+console.log(typeof "yash") // string
+console.log(typeof 18) // number
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
