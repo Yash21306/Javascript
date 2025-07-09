ee96dcf88ca135e1338557d39d66edb797cbcc38
Yash Patel <yppatel21306@gmail.com>
2025-07-09 15:43:43 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber)
+console.log(valueNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1;  false = 0
+
+let isLoggedIn = "YASH"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true;  0 => false
+// "" => false
+// "YASH" => true
+
+let someNumber = 24
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof (stringNumber))
+
