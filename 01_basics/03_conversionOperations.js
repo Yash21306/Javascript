9228055d328bb86b658145666beca95368c324fd
Yash Patel <yppatel21306@gmail.com>
2025-07-09 18:06:56 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,3 +1,4 @@
+/*
 let score = "33abc"
 
 console.log(typeof score)
@@ -20,8 +21,48 @@ console.log(booleanIsLoggedIn)
 // "YASH" => true
 
 let someNumber = 24
-
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof (stringNumber))
+
+*/
+
+
+// ****************************  Operations  *******************************
+
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " Yash"
+console.log(str1 + str2)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
+
+let num1,num2,num3
+num1 = num2 = num3 = 4
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
 
