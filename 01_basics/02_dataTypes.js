03a379979c39aa1aee55515537eea852ba7abe19
Yash Patel <yppatel21306@gmail.com>
2025-07-31 18:37:00 +0000

    datatype summary

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -18,3 +18,55 @@ console.log(typeof 18) // number
 console.log(typeof null) // object
 console.log(typeof undefined) // undefined
 
+
+
+
+// two types of datatype in js
+
+// primitive
+// 7 types: string, number, boolean, null, undefined, symbol, bigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const loggedIn = false
+const outsideTemp = null
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 3654688086543654n   // bigInt
+
+
+
+
+// Non primitive (Reference)
+
+// Array, Objects, Functions
+
+const heroes = ["Thor", "Ironman", "Superman"]
+
+let myObj = {
+    name: "Yash",
+    age: "19"
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof heroes);
+console.log(typeof myObj);
+console.log(typeof myFunction);
+
+
+
+
+
+
+
+
+
